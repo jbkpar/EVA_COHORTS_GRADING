e48e88ce49874bf56963e16193daa41e19cdd293
Jbkpar <jbkpar@gmail.com>
2025-09-08 14:15:16 -0400

    Added teacher

---
+++ src/main/java/EVA_COHORTS_GRADING/entities/Teacher.java
@@ -0,0 +1,8 @@
+package EVA_COHORTS_GRADING.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Teacher extends User {
+
+}
--- src/main/java/EVA_COHORTS_GRADING/entities/User.java
+++ src/main/java/EVA_COHORTS_GRADING/entities/User.java
@@ -1,9 +1,6 @@
 package EVA_COHORTS_GRADING.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -11,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.UniqueElements;
 
+@Inheritance(strategy = InheritanceType.JOINED)
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
