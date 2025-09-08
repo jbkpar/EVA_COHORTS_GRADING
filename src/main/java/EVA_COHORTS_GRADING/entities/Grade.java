d929ad54d8bc79c704ed0b997bc1a8c3ab07d1f2
marcelpeyton <marcel-p@outlook.com>
2025-09-08 13:45:37 -0400

    Update Grade.java

--- src/main/java/EVA_COHORTS_GRADING/entities/Grade.java
+++ src/main/java/EVA_COHORTS_GRADING/entities/Grade.java
@@ -1,11 +1,17 @@
 package EVA_COHORTS_GRADING.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Grade {
 
     @Id
