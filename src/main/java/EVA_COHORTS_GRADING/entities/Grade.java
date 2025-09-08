f167c2e2a54b7e47d95c54a45e05962214e27c88
Marcel Peyton <marcel-p@outlook.com>
2025-09-08 13:42:23 -0400

    Added Grade

---
+++ src/main/java/EVA_COHORTS_GRADING/entities/Grade.java
@@ -0,0 +1,24 @@
+package EVA_COHORTS_GRADING.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public class Grade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long submissionId;
+    private int pointsEarned;
+    private LocalDateTime gradedDate;
+    private long gradedByTeacherId;
+
+
+    private double calculatePercentage(){
+        return 0.00;
+    }
+
+}
