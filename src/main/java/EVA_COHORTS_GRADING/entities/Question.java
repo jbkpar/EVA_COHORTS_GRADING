21dcf230bad20df4300896b908302f86570e331b
Marcel Peyton <marcel-p@outlook.com>
2025-09-08 13:51:54 -0400

    Added Question Class

--- src/main/java/EVA_COHORTS_GRADING/entities/Question.java
+++ src/main/java/EVA_COHORTS_GRADING/entities/Question.java
@@ -1,6 +1,9 @@
 package EVA_COHORTS_GRADING.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private long assignmentId;
     private String questionText;
