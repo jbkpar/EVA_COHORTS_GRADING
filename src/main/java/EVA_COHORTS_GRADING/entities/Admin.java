1e843ffa9bfc6f93894b9b0d03242f6d5a67a573
Marcel Peyton <marcel-p@outlook.com>
2025-09-08 14:12:05 -0400

    Added the Admin Class

--- src/main/java/EVA_COHORTS_GRADING/entities/Admin.java
+++ src/main/java/EVA_COHORTS_GRADING/entities/Admin.java
@@ -1,10 +1,8 @@
 package EVA_COHORTS_GRADING.entities;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
 public class Admin extends User{
 }
